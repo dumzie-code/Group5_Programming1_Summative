303030819e4b83f13ccbc36ced3b1a399949074d
anita20 <a.otoo@alustudent.com>
2026-09-07 02:27:00 +0000

    add main application loop

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+print("========================================")
+print("Clinic Appointment And Management System")
+print("========================================")
 
+def main():  # this particular function will control the main flow
+              #of the application'
 
+    while True:
+        #Keep the system running until the user chooses to exit
+        print("Clinic system is running ...")  
 
+    #Ask the user for their ID or type Exit 
+        user_id =input("Enter your ID or type Exit")
+        
+        if user_id =="Exit":
+            #close the system when the user chooses Exit
+            print("Thank you for using the Clinic Management System.")
+            print("System closed successfully")
+            break
+
+    print("Welcome to the System!")
