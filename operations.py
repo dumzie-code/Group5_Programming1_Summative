024df5580b4f2ec5a3a5f1dd97585b7e4427c673
email <c.ukogu@alustudent.com>
2026-09-09 19:44:26 +0100

    Finalised validating and handling exceptions for the doctors menu, patients menu, admin menu, first name, last name, date, date of birth, phone number

--- operations.py
+++ operations.py
@@ -2,3 +2,5 @@ print("Day one on clinic appointment system!")
 
 
 
+def generate_new_id(role,existing_users):
+    pass 
--- validation.py
+++ validation.py
@@ -10,43 +10,104 @@ def get_valid_menu():
             break
         except ValueError:
             print("Invalid format. Select a number from 1-4 ")       
+            
+def get_valid_admin_menu():
+    #This function displays the options available
+    #to an adminstrator.
+
+    while True:
+        print("=====ADMINISTRATOR MENU=====")
+        print("1.Register new patient")
+        print("2.Display all patients")
+        print("3.Search patient")
+        print("4.Update patient information")
+        print("5.Add doctor")
+        print("6.View appointments")
+        print("7.Logout")
+        
+        try:
+            choice = int(input("Choose an option(1-7): "))
+
+            if choice < 1 or choice > 7:
+                print("Selection out of range. Please select a number from 1-7 ")
+                continue
+            break
+        except ValueError:
+           print("Invalid format. Select a number from 1-7 ")       
+                    
+def get_valid_doctor_menu()  :                 
+    while True:
+        print("=====DOCTOR MENU=====")
+        print("1.View appointment")
+        print("2.View patient information")
+        print("3.Update appointment")
+        print("4.Logout")
+        try:
+            choice = int(input("Choose an option(1-4): "))
+        
+            if choice < 1 or choice > 4:
+                print("Selection out of range. Please select a number from 1-7 ")
+                continue
+            break
+        except ValueError:
+            print("Invalid format. Select a number from 1-4 ")  
+            
+            
+def get_valid_patient_menu():
+    while True:
+        print("========PATIENT MENU========")
+        print("1.View my infomation")
+        print("2.Book appointment")
+        print("3.View my appointment")
+        print("4.Cancel appointment")
+        print("5.Logout")
+        try:
+            choice = int(input("Choose an option(1-5): "))
+                    
+            if choice < 1 or choice > 5:
+                print("Selection out of range. Please select a number from 1-7 ")
+                continue
+            break
+        except ValueError:
+                print("Invalid format. Select a number from 1-5 ")  
         
             
-def get_valid_name():
+def get_valid_firstname():
     while True:
+        try:
             name=input("Please enter your name") 
-            if name:
-                return name  
-            else:
+        except ValueError:
                 print("Invalid format. Please enter a valid name")
+                
+def get_valid_lastname():
+    while True:
+            try:
+                name=input("Please enter your name") 
+            except ValueError:
+                    print("Invalid format. Please enter a valid name")
             
     
 def get_valid_date():
      while True:
-            try:
-                due_date_input= input("Please enter your appointment date")
-                due_date = datetime.strptime(due_date_input, "%Y-%m-%d").date() 
-            except ValueError:
-                pass
-def get_valid_time():
-    while True:
         try:
-            due_date_input= input("Please enter your appointment time")
-            due_date = datetime.strptime(due_date_input, "%Y-%m-%d").date() 
+            date_input= input("Please enter your appointment date")
+            date = datetime.strptime(due_date_input, "%Y-%m-%d").date() 
+            break
         except ValueError:
-            pass
+            print("Invalid format. Please try again with this format date as, YYYY-MM-DD: ")
+        return date
+
 def get_valid_number():
     while True:
         try:
             number=input("Enter your phone number with your country code")
+            
         except ValueError:
             print("Invalid format. Please enter a valid phone number")
-def get_valid_pin():
-    while True:
-        try:
-            pin=input("Enter your pin")
-        except ValueError:
-            print("Invalid format. Please enter a pin")
+        return number
+
+def get_valid_id():
+    pass
     
     
 #
