e151f76574d17b24e175a20bb4d7bd4a97e26627
email <c.ukogu@alustudent.com>
2026-09-12 09:32:50 +0100

    Made updates to my validation file specifically working on the function that will properly validates date inputs

--- validation.py
+++ validation.py
@@ -1,5 +1,5 @@
 print("Day one on clinic appointment system!")
-import datetime
+from datetime import datetime
 def get_valid_menu():
     while True:
         try:
@@ -73,29 +73,44 @@ def get_valid_patient_menu():
         
             
 def get_valid_firstname():
-    while True:
-        try:
-            name=input("Please enter your name") 
-        except ValueError:
-                print("Invalid format. Please enter a valid name")
+   while True:
+    firstname=input("Please enter your first name: ").strip()
+    if firstname=="":
+        print("Oops sorry first name can not be left empty")
+        continue
+    if not firstname.isalpha():
+        print("Invalid format. Please enter a valid name")
+        continue
+    if firstname.isalpha():
+        return  firstname      
                 
 def get_valid_lastname():
     while True:
-            try:
-                name=input("Please enter your name") 
-            except ValueError:
-                    print("Invalid format. Please enter a valid name")
-            
-    
+        lastname=input("Please enter your last name: ").strip()
+        if lastname=="":
+            print("Oops sorry lasst name can not be left empty")
+            continue
+        if not lastname.isalpha():
+            print("Invalid format. Please enter a valid name")
+            continue
+        if lastname.isalpha():
+            return  lastname      
+           
+        
+                        
+                            
 def get_valid_date():
-     while True:
+    while True:
         try:
-            date_input= input("Please enter your appointment date")
+            date_input= input("Please enter your appointment date: ")
             date = datetime.strptime(date_input, "%Y-%m-%d").date() 
+            if date_input=="":
+                print("Oops sorry date can not be left empty enter a valid dat")
+                continue 
             break
         except ValueError:
             print("Invalid format. Please try again with this format date as, YYYY-MM-DD: ")
-        return date
+    return date
 
 def get_valid_number():
     while True:
@@ -108,9 +123,10 @@ def get_valid_number():
 
 def get_valid_id():
     pass
-    
+
+
     
 #
-
+get_valid_date()
 
 
